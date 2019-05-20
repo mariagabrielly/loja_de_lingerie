0239140f70e4f66313a1200755647c9b7b75bcd8
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2019-05-20 11:51:10 +0000

    migrations

--- app.py
+++ app.py
@@ -76,7 +76,6 @@ def index():
     produtos = ProdutoTipo.query.all()
     return render_template('index.html', produtos=produtos)
 
-
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, ProdutoTipo=ProdutoTipo)
